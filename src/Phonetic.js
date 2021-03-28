a4dad5f311db44712f2ed9ec229c2837d03a4a6f
catherine1201 <lcatherine1201@gmail.com>
2021-03-28 21:54:45 +0800

    updated phonetic / audio

---
+++ src/Phonetic.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  return (
+    <div className="phonetic">
+      <a href={props.phonetic.audio}>
+        Pronunciation
+      </a>
+      <br />
+      {props.phonetic.text}
+    </div>
+  )
+}
--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Definitions from "./Definitions";
+import Phonetic from "./Phonetic";
 
 export default function SearchResult(props){
   if (props.results) {
     return (
       <div className="SearchResult">
-      <h2>{props.results.word}</h2>
+        <h2>{props.results.word}</h2>
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
+        <div key={index} >
+          <Phonetic phonetic={phonetic} />
+        </div>
+        );
+        })}
       {props.results.meanings.map(function (meaning, index) {
         return (
           <div key={index}>
